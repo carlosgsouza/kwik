421873e0e8edd956f2904191795244fa0d268213
carlos souza <carlosgsouza@gmail.com>
2013-09-01 01:32:52 -0300

    Installing and configuring Spring Security

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -89,3 +89,8 @@ log4j = {
            'org.hibernate',
            'net.sf.ehcache.hibernate'
 }
+
+// Added by the Spring Security Core plugin:
+grails.plugins.springsecurity.userLookup.userDomainClassName = 'kwik.User'
+grails.plugins.springsecurity.userLookup.authorityJoinClassName = 'kwik.UserRole'
+grails.plugins.springsecurity.authority.className = 'kwik.Role'
